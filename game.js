b0361d383f7dc92da4a1d315b0a48ad44b21c1bb
Divya Ayikkara <divyaayikkara@nbp-109-182.nbp.ruw.rutgers.edu>
2018-01-27 01:55:53 -0500

    inital comment

---
+++ game.js
@@ -0,0 +1,13 @@
+// file that contains the list of words thatt will be randonly selected for the player
+var wordList = ["apple", "banana", "mango", "orange", "strawberries", "peach", "plum"];
+var randomWord = [Math.floor(Math.random()*wordList.length)];
+var choosenWord = wordList[randomWord];
+
+//console.log(choosenWord);
+
+exports.choosenWord = choosenWord;
+
+
+
+
+
